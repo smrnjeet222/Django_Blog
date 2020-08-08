62632eb35e54ca0830ab261013b3d86a10158fbc
Simranjeet Singh <smrnjeet222@gmail.com>
2020-08-08 22:49:51 +0530

    allowed host

--- Django_Blog/settings.py
+++ Django_Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["py3-django-blog.herokuapp.com"]
 
 
 # Application definition
